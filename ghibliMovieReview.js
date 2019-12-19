513276594a04c5b73b33aae239fafb5fe7059d5a
Nilber Remon <nilberremon@gmail.com>
2019-12-18 21:41:06 -0500

    Adding JS

--- ghibliMovieReview.js
+++ ghibliMovieReview.js
@@ -2,25 +2,68 @@ document.addEventListener("DOMContentLoaded", () => {
     
     let select = document.querySelector(".select")
     let form = document.querySelector(".form")
+    let review = document.querySelector("#review")
     let revList = document.querySelector(".revList")
-    let  title = document.querySelector("#title")
+    let title = document.querySelector("#title")
     let release = document.querySelector("#release")
     let description = document.querySelector("#description")
+    let movie = document.querySelector(".movie")
     
     const fetchData = async () => {
         try {
             let res = await axios.get("https://ghibliapi.herokuapp.com/films")
             let data = res.data
-            popMovie(data)
+            popMovieSelect(data)
         } catch(err){
             console.log(err)
         }
     }
+    
     fetchData()
 
-    const popMovie = (data) => {
-        data.forEach(movie =>)
+    const popMovieSelect = (data) => {
+        data.forEach(movie => {
+            let option = document.createElement("option")
+            option.innerText = movie.title
+            option.value = movie.title
+            select.appendChild(option)
+        })
     }
+
+    select.addEventListener("change", (select) => {
+        title.innerText = ""
+        release.innerText = ""
+        description.innerText = ""
+        console.log(select)
+        movieInfo(select.value)
+    })
+
+    const movieInfo = async (selection) => {
+        try {
+            let res = await axios.get("https://ghibliapi.herokuapp.com/films")
+            let movieContent = res.data
+            movieContent.forEach(movie => {
+                if(movie.title === selection){
+                    title.innerText = movie.title
+                    release.innerText = movie.release_date
+                    description.innerText = movie.description
+                  
+                }
+            debugger
+            })
+        }catch(err) {
+            console.log(err)
+        }
+    }
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        let li = document.createElement("li")
+        li.innerText = review.value
+        revList.appendChild(li)
+        form.reset()
+    })
+
 
 
 
